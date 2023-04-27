4dc8bc765b734e91e486faf7378d5dd74b3f68f0
user7 <im.krista69@gmail.com>
2023-04-27 10:06:11 +0300

    устранение замечаний

--- src/js/index.js
+++ src/js/index.js
@@ -1,123 +1,111 @@
 import '../scss/style.scss'
 
 //показать полное меню
-const btnShowMenu = document.getElementById('btn-menu');
+const btnShowMenu = document.querySelector('.show-full-menu');
 btnShowMenu.addEventListener('click', showFullMenu);
 function showFullMenu() {
-  document.getElementById('full-menu').style.display = 'unset';
+  document.querySelector('.full-menu').style.display = 'unset';
 }
 
 //закрыть полное меню
 const blurFullMenu = document.querySelector('.menu-opacity');
 blurFullMenu.addEventListener('click', closeFullMenu);
-const btnCloseMenu = document.getElementById('btn-close-menu');
+const btnCloseMenu = document.querySelector('.close-full-menu');
 btnCloseMenu.addEventListener('click', closeFullMenu);
 function closeFullMenu() {
-  document.getElementById('full-menu').style.display = 'none';
+  document.querySelector('.full-menu').style.display = 'none';
 }
 
 //показать обратную свзять
-const btnShowFeedBack = document.getElementById('chat-feedback');
+const btnShowFeedBack = document.querySelector('.chat');
 btnShowFeedBack.addEventListener('click', showFeedBack);
-const btnGetFeedBack = document.getElementById('get-chat');
+const btnGetFeedBack = document.querySelector('.additional-list__chat');
 btnGetFeedBack.addEventListener('click', showFeedBack);
 function showFeedBack() {
-  document.getElementById('feedback').style.display = 'unset';
-  document.getElementById('callback').style.display = 'none';
+  document.querySelector('.feedback').style.display = 'unset';
+  document.querySelector('.callback').style.display = 'none';
 }
 
 //закрыть обратную связь
-const btnCloseFeedBack = document.getElementById('feedback-close');
+const btnCloseFeedBack = document.querySelector('.close-feedback');
 btnCloseFeedBack.addEventListener('click', closeFeedBack);
 const blurFeedBack = document.querySelector('.feedback-opacity');
 blurFeedBack.addEventListener('click', closeFeedBack);
 function closeFeedBack() {
-  document.getElementById('feedback').style.display = 'none';
+  document.querySelector('.feedback').style.display = 'none';
 }
 
 //показать заказ звонка
-const btnShowCallBack = document.getElementById('call');
+const btnShowCallBack = document.querySelector('.call');
 btnShowCallBack.addEventListener('click', showCallBack);
-const btnGetChat = document.getElementById('get-call');
+const btnGetChat = document.querySelector('.additional-list__call');
 btnGetChat.addEventListener('click', showCallBack);
 function showCallBack() {
-  document.getElementById('callback').style.display = 'unset';
-  document.getElementById('feedback').style.display = 'none';
+  document.querySelector('.callback').style.display = 'unset';
+  document.querySelector('.feedback').style.display = 'none';
 }
 
 //закрыть заказ звонка
-const btnCloseCallBack = document.getElementById('callback-close');
+const btnCloseCallBack = document.querySelector('.close-callback');
 btnCloseCallBack.addEventListener('click', closeCallBack);
 const blurCallBack = document.querySelector('.callback-opacity');
 blurCallBack.addEventListener('click', closeCallBack);
 function closeCallBack() {
-  document.getElementById('callback').style.display = 'none';
+  document.querySelector('.callback').style.display = 'none';
 }
 
 //показать статью о компании
-const btnMoreInfo = document.getElementById('btn-more-info');
-btnMoreInfo.addEventListener('click', {
-  handleEvent(event) {
+const btnMoreInfo = document.querySelector('.read-more');
+btnMoreInfo.addEventListener('click',function(event) {
     document.querySelector('.block__infoCPS').style.overflow = 'visible';
     document.querySelector('.block__infoCPS').style.height = 'auto'; //'100%';
-    document.getElementById('btn-more-info').style.display = 'none';
-    document.getElementById('btn-less-info').style.display = 'unset';
-  }
+    document.querySelector('.read-more').style.display = 'none';
+    document.querySelector('.read-less').style.display = 'unset';
 });
 
 //скрыть статью о компании
-const btnLessInfo = document.getElementById('btn-less-info');
-btnLessInfo.addEventListener('click', {
-  handleEvent(event) {
+const btnLessInfo = document.querySelector('.read-less');
+btnLessInfo.addEventListener('click', function(event) {
     document.querySelector('.block__infoCPS').style.overflow = '';
     document.querySelector('.block__infoCPS').style.height = '';
-    document.getElementById('btn-more-info').style.display = 'unset';
-    document.getElementById('btn-less-info').style.display = 'none';
-  }
+    document.querySelector('.read-more').style.display = 'unset';
+    document.querySelector('.read-less').style.display = 'none';
 });
 
 //бренды показать
-const btnMoreBrands = document.getElementById('btn-more');
-btnMoreBrands.addEventListener('click', {
-  handleEvent(event) {
+const btnMoreBrands = document.querySelector('.show-more');
+btnMoreBrands.addEventListener('click', function(event) {
     document.querySelector('.list-logo').style.overflow = 'visible';
     document.querySelector('.list-logo').style.height = '100%';
-    document.getElementById('btn-more').style.display = 'none';
-    document.getElementById('btn-less').style.display = 'unset';
-  }
+    document.querySelector('.show-more').style.display = 'none';
+    document.querySelector('.show-less').style.display = 'unset';
 });
 
 //бренды скрыть
-const btnLessBrands = document.getElementById('btn-less');
-btnLessBrands.addEventListener('click', {
-  handleEvent(event) {
+const btnLessBrands = document.querySelector('.show-less');
+btnLessBrands.addEventListener('click', function(event) {
     document.querySelector('.list-logo').style.overflow = '';
     document.querySelector('.list-logo').style.height = '';
-    document.getElementById('btn-more').style.display = 'unset';
-    document.getElementById('btn-less').style.display = 'none';
-  }
+    document.querySelector('.show-more').style.display = 'unset';
+    document.querySelector('.show-less').style.display = 'none';
 });
 
 //Сервисы показать
-const btnMoreServices = document.getElementById('btn-more-services');
-btnMoreServices.addEventListener('click', {
-  handleEvent(event) {
+const btnMoreServices = document.querySelector('.btn-more-services');
+btnMoreServices.addEventListener('click', function(event) {
     document.querySelector('.list-services').style.overflow = 'visible';
     document.querySelector('.list-services').style.height = '100%';
-    document.getElementById('btn-more-services').style.display = 'none';
-    document.getElementById('btn-less-services').style.display = 'unset';
-  }
+    document.querySelector('.btn-more-services').style.display = 'none';
+    document.querySelector('.btn-less-services').style.display = 'unset';
 });
 
 //Сервисы скрыть
-const btnLessServices = document.getElementById('btn-less-services');
-btnLessServices.addEventListener('click', {
-  handleEvent(event) {
+const btnLessServices = document.querySelector('.btn-less-services');
+btnLessServices.addEventListener('click', function(event) {
     document.querySelector('.list-services').style.overflow = '';
     document.querySelector('.list-services').style.height = '';
-    document.getElementById('btn-more-services').style.display = 'unset';
-    document.getElementById('btn-less-services').style.display = 'none';
-  }
+    document.querySelector('.btn-more-services').style.display = 'unset';
+    document.querySelector('.btn-less-services').style.display = 'none';
 });
 
 // slider
